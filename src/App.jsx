27d24c1ba499f19e5f8667968008095f6dd16f8f
Ayush Kumar Mishra <ayushkumar.12212147@lpu.in>
2024-06-18 15:09:56 +0530

    tenth

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import Home from '../src/Components/HomePage/HomePage';
 import './App.css';
 import Teck from '../src/Components/TechStack/TechStack';
 import Project from '../src/Components/Projects/Projects';
+import About from '../src/Components/About/About';
 function App() {
   return (
     <div className='app'>
       <NavBar/>
       <Home/>
       <Teck/>
+      
       <Project/>
+      
+      <About/>
     </div>
   )
 }
---
+++ src/Components/About/About.jsx
@@ -0,0 +1,46 @@
+import './About.css'
+function About(){
+    return (
+        <div className="about_me">
+        <div className="skill2">
+            <h1>About <span className='green'>Me</span></h1>
+
+            <li>
+                <h3>Java</h3>
+                <span className='bar'><span className='java'></span></span>
+            </li>
+            <li>
+                <h3>C</h3>
+                <span className='bar'><span className='c'></span></span>
+            </li>
+            <li>
+                <h3>CPP</h3>
+                <span className='bar'><span className='cpp'></span></span>
+            </li>
+            <li>
+                <h3>Javascript</h3>
+                <span className='bar'><span className='javascript'></span></span>
+            </li>
+            <li>
+                <h3>React</h3>
+                <span className='bar'><span className='react'></span></span>
+            </li>
+            <li>
+                <h3>Mern</h3>
+                <span className='bar'><span className='mern'></span></span>
+            </li>
+            <li>
+                <h3>Angular</h3>
+                <span className='bar'><span className='angular'></span></span>
+            </li>
+        </div>
+        <div className="para">
+        <p>Welcome to my portfolio! I am a dedicated B.Tech student specializing in Computer Science and Engineering, with a passion for coding and development. <br /> My academic journey has equipped me with a solid foundation in programming languages, software development, and problem-solving skills. <br /> I have successfully completed various projects that showcase my abilities in web development, app creation, and algorithm design. My work is driven by creativity, innovation, and a commitment to excellence. <br /> I am always eager to tackle new challenges and learn new technologies. Feel free to explore my projects and reach out if you would like to collaborate or learn more about my work.</p>
+        </div>
+
+        </div>
+        
+    )
+
+}
+export default About;
